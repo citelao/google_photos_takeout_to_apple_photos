adfb16efea1cd3cdb6b5a1beedecd3974185381b
Ben Stolovitz <ben@stolovitz.com>
2022-02-20 21:39:23 -0800

    don't add []

--- src/Logger.ts
+++ src/Logger.ts
@@ -4,12 +4,20 @@ export default class Logger {
     public static LOG_FILE = `log-${new Date().toISOString()}.log`;
 
     public static log(t?: unknown, ...params: unknown[]) {
-        console.log(t, params);
+        if (params.length > 0) {
+            console.log(t, params);
+        } else {
+            console.log(t);
+        }
         fs.appendFileSync(this.LOG_FILE, JSON.stringify([t, ...params], undefined, 4));
     }
 
     public static warn(t?: unknown, ...params: unknown[]) {
-        console.warn(t, params);
+        if (params.length > 0) {
+            console.warn(t, params);
+        } else {
+            console.warn(t);
+        }
         fs.appendFileSync(this.LOG_FILE, JSON.stringify(["WARN", t, ...params], undefined, 4));
     }
 }
