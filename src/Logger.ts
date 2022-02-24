ddd8696dc4f4441c032de82f0868abdb26eb8d68
Ben Stolovitz <ben@stolovitz.com>
2022-02-24 00:04:15 -0800

    VERBOSE

--- src/Logger.ts
+++ src/Logger.ts
@@ -40,4 +40,20 @@ export default class Logger {
         }
         fs.appendFileSync(this.LOG_FILE, JSON.stringify(["WARN", t, ...params], undefined, 4));
     }
+
+    public static verbose(t?: unknown, ...params: unknown[]) {
+        if (!t) {
+            fs.appendFileSync(this.LOG_FILE, "\n");
+            return;
+        }
+
+        const items = [t, ...params].map((i) => {
+            if (typeof i === "string") {
+                return stripAnsi(i);
+            }
+
+            return i;
+        });
+        fs.appendFileSync(this.LOG_FILE, JSON.stringify(items, undefined, 4));
+    }
 }
