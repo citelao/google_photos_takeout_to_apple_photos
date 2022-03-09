10e72f7a679ba221cfa7e871db35f460c8ad7793
Ben Stolovitz <ben@stolovitz.com>
2022-03-08 20:13:21 -0800

    internal info

--- contrib/inspector.ts
+++ contrib/inspector.ts
@@ -1,15 +1,20 @@
 import { program } from "commander";
-import { getPropertiesForPhotoWithId, getPropertiesForSelection, launchPhotosWithId } from "../src/photos_app";
+import { getInfoForPhotoIds, getPropertiesForPhotoWithId, getPropertiesForSelection, launchPhotosWithId } from "../src/photos_app";
 
 program
     .argument('[media item id]', 'Photos media item ID')
     .option('-i --info', 'Get info for item')
+    .option('-n --internal', 'Get internal info for item')
     .action(async (media_item) => {
         if (!media_item) {
             console.log(getPropertiesForSelection());
         } else {
             if (program.opts().info) {
-                console.log(getPropertiesForPhotoWithId(media_item));
+                if (program.opts().internal) {
+                    console.dir(getInfoForPhotoIds([media_item]));
+                } else {
+                    console.log(getPropertiesForPhotoWithId(media_item));
+                }
             } else {
                 launchPhotosWithId(media_item);
             }
